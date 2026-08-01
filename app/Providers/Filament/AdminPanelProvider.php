f111e5c37a357542106dffd9deaf177c42374a55
Zohaib Akhtar <147222210+zaibiiu@users.noreply.github.com>
2026-08-01 16:35:05 +0500

    changes

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->profile()
             ->brandName('Staff Portal')
             ->colors([
                 'primary' => Color::Blue,
